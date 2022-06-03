f189dd385b200271c4f90e9e94999d9f57abd60e
Kobzar DV <kdmitriyv@bk.ru>
2022-06-03 23:27:14 +0300

    Добавьте файлы проекта.

---
+++ ARA/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VkBotFramework;
+
+namespace ARA
+{
+    class Server
+    {
+        public static string url = "https://vk.com/ara_test";
+        public static VkBot bot;
+
+        public void Start(string token)
+        {
+            bot = new VkBot(token, url, longPollTimeoutWaitSeconds: 0);
+        }
+    }
+}
